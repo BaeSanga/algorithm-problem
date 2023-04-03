be15e1856ded8f6fd649640017b8df1ed965fa0c
SangA77 <tkddk1771@gmail.com>
2023-04-04 02:17:10 +0900

    feat: 04-03 첫번째 커밋

---
+++ problem/04-03/문제_Array.js
@@ -0,0 +1,79 @@
+// Array
+
+// 문제1
+// let input_data = ["웹페", "이지", "단순", "한보", "조기"];
+// input_data.splice(2, 4);
+// console.log(input_data);
+
+// let output_data = input_data.join("");
+// console.log(output_data); // 웹페이지
+//   output = 웹페이지
+
+// 문제2
+// let input_data = ["웹페", "이지", "단순", "한보", "조기"];
+// input_data.splice(0, 2);
+// console.log(input_data);
+
+// let output_data = input_data.push("화보");
+// console.log(output_data); // ["단순", "화보"]
+
+// 문제3
+// {
+//   let input_data = [1, 2, 3, 4, 5];
+//   console.log(output_data); // 5
+// }
+
+// let input_data = [1, 2, 3, 4, 5];
+// let output_data = input_data.pop();
+
+// console.log(output_data);
+
+// 문제4
+// {
+//   // for 사용
+//   let input_data = [1, 2, 3, 4, 5];
+//   console.log(input_data); // [11,12,13,14,15]
+// }
+let input_data = [1, 2, 3, 4, 5];
+
+for (i = 0; i < input_data.length; i++) {
+  console.log(input_data[i] + 10);
+}
+
+// {
+//   // forEach 사용
+//   let input_data = [10, 20, 30, 40, 50];
+//   console.log(input_data); // [5, 10, 15, 20, 25]
+// }
+
+// {
+//   // map 사용
+//   let input_data = [10, 20, 30, 40, 50];
+//   console.log(output_data); // [1, 2, 3, 20, 25]
+// }
+
+// {
+//   let input_data = [10, 20, 30, 40, 50];
+//   console.log(output_data); // [30, 40]
+// }
+
+// {
+//   let input_data = [10, 20, 30, 40, 50];
+//   console.log(input_data); // [10, 20, 50]
+// }
+
+// {
+//   let input_data = [10, 20, 30, 40, 50];
+//   console.log(output_data); // 3 *인덱스 번호
+// }
+
+// {
+//   let input_data = "오늘 날씨는 매우 맑음";
+//   console.log(output_data); // ["오늘", "날씨는", "매우", "흐림"]
+// }
+
+// {
+//   // '=' 금지
+//   let input_data = "오늘 날씨는 매우 맑음";
+//   console.log(output_data); // ["내일", "날씨는", "매우", "맑음]
+// }
