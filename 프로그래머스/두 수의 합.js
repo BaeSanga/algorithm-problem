b09993d424fd9a1438b7005f8f18141186e0ba39
SangA77 <tkddk1771@gmail.com>
2023-04-05 23:08:24 +0900

    feat: 04-05 코테 문제 풀기

--- 프로그래머스/두 수의 합.js
+++ 프로그래머스/두 수의 합.js
@@ -8,5 +8,6 @@ function solution(num1, num2) {
   if (num1 >= -50000 && num1 <= 50000 && num2 >= -50000 && num2 <= 50000) {
     answer = num1 + num2;
   }
+
   return answer;
 }
--- 프로그래머스/몫 구하기.js
+++ 프로그래머스/몫 구하기.js
@@ -8,5 +8,6 @@ function solution(num1, num2) {
   if (num1 >= 0 && num1 <= 100 && num2 >= 0 && num2 <= 100) {
     answer = Math.floor(num1 / num2);
   }
+
   return answer;
 }
