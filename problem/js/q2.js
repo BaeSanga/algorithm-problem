6367047eb481f528e75103233d8637e397a833c0
SangA77 <tkddk1771@gmail.com>
2023-04-09 22:54:20 +0900

    feat: 04-09

--- problem/js/q2.js
+++ problem/js/q2.js
@@ -7,13 +7,17 @@ ex) 길이기 80인 배열은 길이가 5로 분해한다면 16개의 배열을 
     ex) [1...80] => [[1...5], [6...10], [11...15], ... , [76...80]]
 */
 
-var arr = [
+let arr = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25, 26, 27, 28, 29, 30,
 ];
-var result = [];
 
-for (i = 0; i < arr.length; i += 5) {
-  result.push(arr.slice(i, i + 5));
+function quiz2(arr, arrNumber) {
+  let result = [];
+
+  for (i = 0; i < arr.length; i += arrNumber) {
+    result.push(arr.slice(i, i + arrNumber));
+  }
+  return result;
 }
-console.log(result);
+console.log(quiz2(arr, 5));
